2700a0501e7cdfcaf23eb7b2be6fd4718b8e05c9
弈秋 <xing_yi_bin@163.com>
2022-11-09 22:55:11 +0800

    100dayChange

--- src/pages/day100/index.jsx
+++ src/pages/day100/index.jsx
@@ -24,7 +24,7 @@ const Index = () => {
           <div className={style.dd}>
             <div className={cn(style.light, style.left)}></div>
             {/* 实现方法一:障眼法 */}
-            <div className={style.right}>
+            {/* <div className={style.right}>
               <div className={style.circleBox}>
                 <div className={style.circle}></div>
               </div>
@@ -34,8 +34,27 @@ const Index = () => {
               <div className={style.bottomDotBox}>
                 <div className={cn(style.light, style.dot)}></div>
               </div>
-            </div>
+            </div> */}
             {/* 实现方法二:svg大法 */}
+            <svg
+              width="120px"
+              height="120px"
+              viewBox="0 0 120 120"
+              className={style.rightSVG}
+            >
+              <defs>
+                <filter id="glow" x="0" y="0" width="200%" height="200%">
+                  <feOffset result="offOut" in="SourceAlpha" dx="20" dy="20" />
+                  <feGaussianBlur
+                    result="blurOut"
+                    in="offOut"
+                    stdDeviation="10"
+                  />
+                  <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
+                </filter>
+              </defs>
+              <circle cx="60" cy="60" r="56" />
+            </svg>
           </div>
         </div>
       </div>
